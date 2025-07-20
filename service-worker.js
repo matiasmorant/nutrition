09e7db52d2d8e613f0066da444a94808b8f8bbb5
matiasmorant <matias.morant.mm@gmail.com>
2025-07-20 16:12:03 -0300

    PWA

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'nutri-finder-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Add other core assets like CSS/JS files if needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
